fe6cfbe32604760bf59e191d454baed4c9b1e3b9
Lento <lento@channel.io>
2022-12-16 15:50:13 +0900

    add test_output on ecr module (#1)
    * add test_output on ecr module
    
    * working-dir . 로 수정
    
    * Delete test module
    
    * working-dir -> find-dir
    
    * recursive enabled: false
    
    * example/example.tf -> example.tf 위치 수정
    
    * find-dir . -> ./modules
    
    * terraform-docs: automated action
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- modules/ecr/outputs.tf
+++ modules/ecr/outputs.tf
@@ -2,3 +2,7 @@ output "repository_url" {
   description = "Repository URL"
   value       = aws_ecr_repository.this.repository_url
 }
+
+output "test_output" {
+  value = aws_ecr_repository.this.name
+}
